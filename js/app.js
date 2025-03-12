7a08627b98317f9a5476c060efdfb03a56be336f
bonnie <owu.owu.666@gmail.com>
2025-03-12 14:10:35 -0400

    some shit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-// /*-------------------------------- Constants --------------------------------*/
-const board = document.querySelector(".board");
-let squareEls = document.querySelectorAll(".sqr");
-let messageEl = document.querySelector("#message");
+/*-------------------------------- Constants --------------------------------*/
 const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -12,91 +9,63 @@ const winningCombos = [
     [1, 4, 7],
     [2, 5, 8],
   ]
-  
+/*---------------------------- Variables (state) ----------------------------*/
+let board = ["","","","","","","","",""]
+let turn = "X"
+let winner = false
+let tie = false
 
-// /*---------------------------- Variables (state) ----------------------------*/
-let opWinner = false
-let winner = false;
-tie = false;
-let count = 0;
-let gameBoard =[ 
-         "","","",
-         "","","",
-         "","",""
-]
-
-// /*------------------------ Cached Element References ------------------------*/
-// /*-------------------------------- Functions --------------------------------*/
-const check = () => {
- 
-    winningCombos.forEach((combo,index)=>{
-        count = 0
-        combo.forEach((c)=>{
-            if( gameBoard[c]==="X"){
-                count+=1
-            if (count === 3){
-                winner = true
-            }
-            }
+/*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll(".sqr")
+const messageEl = document.querySelector("#message");
+const gameBoard = document.querySelector(".board")
+// console.log(squareEls);
+/*-------------------------------- Functions --------------------------------*/
+const checkForWinner =()=>{
+    winningCombos.forEach = (element)=>{
+        element.forEach = (element)=>{
             
-        })
+        }
+    }
+}
+const placePieces = (index) =>{
+    board[index]= turn 
+}
+gameBoard.addEventListener("click", (event) => {
+    const squareindex =event.target.id
+    if (board[squareindex]!== ""|| winner === true){
+        return
+    }
+    placePieces(squareindex)
+})
+const updateMessage = ()=>{
+    if (winner === false && tie === false){
+        messageEl.innerText = `it is ${turn}'s turn`
+    }
+    else if (winner === false && tie === true)
+        messageEl.innerText = "you tied"
+    else {
+        messageEl.innerText = "you won"
+    }
+}
+const handleClick = (event)=>{
+
+}
+const updateBoard = ()=>{
+    board.forEach((element,index)=>{
+        // console.log(squareEls[index])
+        squareEls[index].innerText = element
     })
 }
-const checkOp = () => {
- 
-    winningCombos.forEach((combo,index)=>{
-        count = 0
-        combo.forEach((c)=>{
-            if( gameBoard[c]==="O"){
-                count+=1
-            if (count === 3){
-                Opwinner = true
-            }
-            }
-            
-        })
-    })
+const render =()=>{
+updateBoard()
+updateMessage()
+}
+const init =()=>{
+
+    render()
 }
 /*----------------------------- Event Listeners -----------------------------*/
-board.addEventListener("click", (event) => {
-  event.target.innerText = "X";
-gameBoard[(Number(event.target.id))] = ("X")
-  for (i = 1; i > 0; ) {
-    oponentMove = Math.floor(Math.random() * 9);
-    if (document.getElementById(oponentMove).innerText === "") {
-      document.getElementById(oponentMove).innerText = "O";
-      count += 1;
-      gameBoard[oponentMove] = "O"
-      break;
-    }
-    if (count >= 4) {
-      break;
-    } else {
-      continue;
-    }
-  }
-check()
-checkOp()
-console.log(gameBoard)
-console.log(winner)
-if (winner === true){
-    messageEl.innerText = "YOU WON!!!!"
-    for(i = 0;i<=9;i++){
-        document.getElementById(i).innerText =""
-    }
-}
-if (opWinner === true){
-    messageEl.innerText = "you lost !!"
-    for(i = 0;i<=9;i++){
-        document.getElementById(i).innerText =""
-    }
-}
-
-});
-// board.addEventListener("click",(event) => {
-//     console.log(event.target.id)
-// })
-// document.getElementById(`#${Math.floor(Math.random() *9)}`).innerText = 'O'
 
 //1) Define the required variables used to track the state of the game.
 
@@ -112,3 +81,5 @@ if (opWinner === true){
 //6) Handle a player clicking a square with a `handleClick` function.
 
 //7) Create Reset functionality.
+init()
+updateBoard()
